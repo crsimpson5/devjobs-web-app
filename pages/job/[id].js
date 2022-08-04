7d6fb64d4a72a0654c749192de14cfecc3592d15
Curtis Simpson <crsimpson5@gmail.com>
2022-08-04 09:09:32 -0500

    Update job page

--- pages/job/[id].js
+++ pages/job/[id].js
@@ -24,70 +24,78 @@ export default function Job() {
 
       <Header />
 
-      {job ? (
-        <>
-          <header className="job__header | container-md">
-            <div
-              className="job__logo"
-              style={{ background: job.logoBackground }}
-            >
-              <img src={`.${job.logo}`} alt="" />
-            </div>
-            <div className="flow" style={{ "--flow-spacing": ".25em" }}>
-              <p className="heading-2">{job.company}</p>
-              <p className="color-light">
-                {job.company.split(" ").join("").toLowerCase() + ".com"}
-              </p>
-            </div>
-            <a
-              href="#"
-              className="button"
-              data-type="light"
-              onClick={(e) => e.preventDefault()}
-            >
-              Company Site
-            </a>
-          </header>
+      <div className="main-content">
+        {job ? (
+          <>
+            <header className="job__header | container-md">
+              <div
+                className="job__logo"
+                style={{ background: job.logoBackground }}
+              >
+                <img src={`.${job.logo}`} alt="" />
+              </div>
+              <div className="flow" style={{ "--flow-spacing": ".25em" }}>
+                <p className="heading-2">{job.company}</p>
+                <p className="color-light">
+                  {job.company.split(" ").join("").toLowerCase() + ".com"}
+                </p>
+              </div>
+              <a
+                href="#"
+                className="button"
+                data-type="light"
+                onClick={(e) => e.preventDefault()}
+              >
+                Company Site
+              </a>
+            </header>
 
-          <main className="container-md color-light">
-            <p className="color-light">
-              {job.postedAt}{" "}
-              <span className="dot-separator"> {job.contract}</span>
-            </p>
-            <h1 className="heading-1">{job.position}</h1>
-            <p className="color-primary bold">{job.location}</p>
-            <button className="button">Apply Now</button>
+            <main className="container-md color-light">
+              <div className="flex align-center justify-between">
+                <div className="flow" style={{ "--flow-spacing": ".25rem" }}>
+                  <p className="color-light">
+                    {job.postedAt}{" "}
+                    <span className="dot-separator"> {job.contract}</span>
+                  </p>
+                  <h1 className="heading-1">{job.position}</h1>
+                  <p className="color-primary bold">{job.location}</p>
+                </div>
+                <button className="button">Apply Now</button>
+              </div>
 
-            <div className="flow">
-              <p>{job.description}</p>
-              <h2 className="heading-2">Requirements</h2>
-              <p>{job.requirements.content}</p>
-              <ul>
-                {job.requirements.items.map((item, idx) => (
-                  <li key={idx}>{item}</li>
-                ))}
-              </ul>
-              <h2 className="heading-2">What You Will Do</h2>
-              <p>{job.role.content}</p>
-              <ol>
-                {job.role.items.map((item, idx) => (
-                  <li key={idx}>{item}</li>
-                ))}
-              </ol>
-            </div>
-          </main>
+              <div className="flow">
+                <p>{job.description}</p>
+                <h2 className="heading-2">Requirements</h2>
+                <p>{job.requirements.content}</p>
+                <ul className="flow" style={{ "--flow-spacing": ".75rem" }}>
+                  {job.requirements.items.map((item, idx) => (
+                    <li key={idx}>{item}</li>
+                  ))}
+                </ul>
+                <h2 className="heading-2">What You Will Do</h2>
+                <p>{job.role.content}</p>
+                <ol className="flow" style={{ "--flow-spacing": ".75rem" }}>
+                  {job.role.items.map((item, idx) => (
+                    <li key={idx}>{item}</li>
+                  ))}
+                </ol>
+              </div>
+            </main>
 
-          <footer>
-            <div className="container-md">
-              <p className="heading-2">{job.position}</p>
-              <p>{job.company}</p>
-              <button className="button">Apply Now</button>
-            </div>
-          </footer>
-        </>
-      ) : (
-        <div>no job found</div>
-      )}
+            <footer>
+              <div className="">
+                <div>
+                  <p className="heading-2">{job.position}</p>
+                  <p>{job.company}</p>
+                </div>
+                <button className="button">Apply Now</button>
+              </div>
+            </footer>
+          </>
+        ) : (
+          <div>no job found</div>
+        )}
+      </div>
     </>
   );
 }
